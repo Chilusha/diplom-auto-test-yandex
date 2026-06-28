4ee4e2715a0ab948b64440328d5448af37d58e5e
Chilusha <Chilusha8@yandex.ru>
2026-06-28 19:57:42 +0300

    Добавлены config, data, и обновлён README

---
+++ config.py
@@ -0,0 +1,3 @@
+BASE_URL = "https://c0322d97-30ac-472a-ad91-79efa0aa64eb.serverhub.praktikum-services.ru"
+ORDERS_ENDPOINT = "/api/v1/orders"
+ORDER_TRACK_ENDPOINT = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,14 @@
+# data.py
+
+def get_order_data():
+    return {
+        "firstName": "Иван",
+        "lastName": "Петров",
+        "address": "ул. Дмитровская, д. 1",
+        "metroStation": "14",
+        "phone": "+7 999 123 45 67",
+        "rentTime": 5,
+        "deliveryDate": "2026-06-30",
+        "comment": "Автотест",
+        "color": ["BLACK"]
+    }
